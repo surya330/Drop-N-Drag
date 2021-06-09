1fd95c6cceb1f81ef9d7e7923053af5c94947eaf
Surya Teja Chatrathi <77150730+surya330@users.noreply.github.com>
2021-06-09 19:30:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const fill = document.querySelector('.fill')
+const boxes = document.querySelectorAll('.box')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const box of boxes) {
+    box.addEventListener('dragover', dragOver)
+    box.addEventListener('dragenter', dragEnter)
+    box.addEventListener('dragleave', dragLeave)
+    box.addEventListener('drop', dragDrop)
+}
+
+function dragStart() {
+    this.className += ' hold' 
+    setTimeout(() => this.className = 'invisible', 0)
+}
+
+function dragEnd() {
+    this.className = 'fill'
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    this.className = 'box'
+}
+
+function dragDrop() {
+    this.className = 'box'
+    this.append(fill)
+}
